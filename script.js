63efe3b12b2f7ee30fc0f318245c86f76e4f27de
trix-04 <jxt_04@outlook.com>
2024-11-28 03:13:02 -0500

    Created an object ‘student’ that displays all the properties listed within  part 1 of instructions and console logged specific properties.

---
+++ script.js
@@ -0,0 +1,14 @@
+let student = {
+    name: "Trish",
+    age: 27 ,
+    enrolled: true,
+    courses: [ "CPAN134", "PSY230", "CPAN116", "SCIE223" ],
+    display: () => { 
+        console.log(`Basic Student Info: \nName: ${this.name}\nAge: ${this.age}\nEnrolled: ${this.enrolled}\nCourses: ${this.courses}`);
+    }
+}
+    
+    console.log(`The student's name is ${student.name} and they are ${student.age} years old`);
+    student.display();
+    
+    
